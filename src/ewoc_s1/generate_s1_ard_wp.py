5b6c71d0151b6d14792941c6915333b5117823e6
Mickael Savinaud <mickael.savinaud@c-s.fr>
2021-07-23 09:58:25 +0200

    wip wp

---
+++ src/ewoc_s1/generate_s1_ard_wp.py
@@ -0,0 +1,166 @@
+
+import argparse
+import logging
+from pathlib import Path
+import sys
+
+from dataship.dag.utils import get_product_by_id
+from s1tiling.S1Processor import main as s1_process
+
+from ewoc_s1 import __version__
+from ewoc_s1.s1_prd_id import S1PrdIdInfo
+from ewoc_s1.ewoc_s1_ard import to_ewoc_s1_ard
+from ewoc_s1.utils import EwocWorkPlanReader, to_s1tiling_configfile
+
+__author__ = "Mickael Savinaud"
+__copyright__ = "Mickael Savinaud"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+def generate_s1_ard_wp(work_plan_filepath, out_dirpath, conf_filepath, dem_dirpath=None, working_dirpath_root=None):
+    working_dirpath = working_dirpath_root / 'ewoc_s1_wp'
+    working_dirpath.mkdir(exist_ok=True)
+    _logger.info('Work plan: %s', work_plan_filepath)
+    _logger.info('out_dirpath: %s', out_dirpath)
+    _logger.info('dem_dirpath: %s', dem_dirpath)
+    _logger.info('working_dirpath: %s', working_dirpath)
+
+
+
+    # Extract from workplan each S1 product id for each tile to process
+    wp_reader = EwocWorkPlanReader(work_plan_filepath)
+    _logger.info(wp_reader.tile_ids)
+    
+    for s2_tile_id in wp_reader.tile_ids:
+        #_logger.info(wp_reader.get_s1_prd_ids(s2_tile_id))
+        # TODO manage S1 product to retrieve date by date
+
+        s1_input_dirpath_tile = working_dirpath / 'input' / s2_tile_id
+        s1_input_dirpath_tile.mkdir(exist_ok=True)
+        
+        #for s1_prd_id in wp_reader.get_s1_prd_ids(s2_tile_id):
+        for date_key, s1_prd_ids in wp_reader.get_s1_prd_ids_by_date(s2_tile_id).items():
+            s1_input_dirpath_tile_date = s1_input_dirpath_tile / date_key
+            s1_input_dirpath_tile_date.mkdir(exist_ok=True)
+            out_dirpath_date = out_dirpath / date_key
+            for s1_prd_id in s1_prd_ids: 
+                if S1PrdIdInfo.is_valid(s1_prd_id):
+                    s1_prd_safe_dirpath = s1_input_dirpath_tile_date / s1_prd_id
+                    s1_prd_wsafe_dirpath =  s1_input_dirpath_tile_date / s1_prd_safe_dirpath.stem
+                    if not s1_prd_wsafe_dirpath.exists():
+                        get_product_by_id(s1_prd_id, s1_input_dirpath_tile_date, 'creodias', config_file=conf_filepath)
+                        s1_prd_safe_dirpath.rename(s1_prd_wsafe_dirpath)
+                    else:
+                        _logger.info('S1 prd %s is already available on disk', s1_prd_id)
+                else:
+                    _logger.warning('S1 prd id %s is not valid!', s1_prd_id)
+
+    
+            s1_process.callback(20, False, False, False, False, False,
+                                to_s1tiling_configfile(out_dirpath_date,
+                                                s1_input_dirpath_tile_date,
+                                                dem_dirpath,
+                                                working_dirpath,
+                                                s2_tile_id))
+
+            to_ewoc_s1_ard( out_dirpath_date / s2_tile_id, out_dirpath, 
+                            S1PrdIdInfo(s1_prd_ids[0]), s2_tile_id, 
+                            rename_only=False)
+
+        # TODO Push to bucket if requested
+
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Generate EWoC S1 ARD from a EWOC workplan")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="ewoc_s1 {ver}".format(ver=__version__),
+    )
+    parser.add_argument(dest="work_plan", help="EWoC Working plan", type=Path)
+    parser.add_argument(dest="out_dirpath", help="Output Dirpath", type=Path)
+    parser.add_argument("--dem_dirpath", dest="dem_dirpath", help="DEM dirpath", type=Path)
+    parser.add_argument("-w", dest="working_dirpath", help="Working dirpath", type=Path)
+    parser.add_argument("-c", dest="conf_filepath", help="eodag conf filepath", type=Path)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`generate_s1_ard_wp` to be called with string arguments in a CLI fashion
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting Generate S1 ARD for the workplan ...", args.work_plan)
+    generate_s1_ard_wp(args.work_plan, args.out_dirpath, args.conf_filepath,
+                       args.dem_dirpath, args.working_dirpath)
+    _logger.info("Generation of the EWoC workplan %s for S1 part is ended!", args.work_plan)
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m ewoc_s1.generate_ard 42
+    #
+    run()
--- src/ewoc_s1/utils.py
+++ src/ewoc_s1/utils.py
@@ -1,9 +1,13 @@
 import configparser
+import json
 
-# TODO manage processing parameters
+from ewoc_s1.s1_prd_id import S1PrdIdInfo
+
 
 def to_s1tiling_configfile(out_dirpath, s1_input_dirpath, dem_dirpath, working_dirpath, s2_tile_id):
-    
+# TODO manage processing parameters
+
+
     config = configparser.ConfigParser()
     config['Paths'] = {'output': str(out_dirpath),
                        's1_images': str(s1_input_dirpath),
@@ -35,3 +39,37 @@ def to_s1tiling_configfile(out_dirpath, s1_input_dirpath, dem_dirpath, working_d
         config.write(configfile)
 
     return config_filepath
+
+class EwocWorkPlanReader():
+    PROD_TYPE = {"S2":"S2_PROC","L8":"L8_PROC","S1":"SAR_PROC"}    
+
+    def __init__(self, workplan_filepath) -> None:
+        with open(workplan_filepath) as f:
+            self._wp = json.load(f)
+        
+        self._tile_ids = list()
+        for tile in self._wp:
+            self._tile_ids.append(tile)
+
+        # split name to retrieve start_date and end_date
+
+    @property
+    def tile_ids(self):
+        return self._tile_ids
+
+    def get_s1_prd_ids(self, tile_id):
+        if tile_id in self._tile_ids:
+            return self._wp[tile_id]['SAR_PROC']['INPUTS']
+        else:
+            return None
+    
+    def get_s1_prd_ids_by_date(self, tile_id):
+        prd_ids = self.get_s1_prd_ids(tile_id)
+        out = dict()
+        for prd_id in prd_ids:
+            date_key = str(S1PrdIdInfo(prd_id).start_time.date())
+            if date_key in out.keys():
+                out[date_key] = out.get(date_key) + [prd_id]
+            else:
+                out[date_key] = [prd_id]
+        return out
