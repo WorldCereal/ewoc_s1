2a6582ced90e7e6faf78dc16275e726dd5894871
Aurelien Bricier <aurelien.bricier@csgroup.eu>
2021-08-18 16:48:58 +0200

    FEAT: Added local bucket srtm support

--- src/ewoc_s1/generate_s1_ard_pid.py
+++ src/ewoc_s1/generate_s1_ard_pid.py
@@ -18,10 +18,10 @@ __license__ = "MIT"
 logger = logging.getLogger(__name__)
 
 
-def generate_s1_ard_from_pids(s1_prd_ids, s2_tile_id, out_dirpath_root,  
-                        dem_dirpath=None, working_dirpath_root=None, 
+def generate_s1_ard_from_pids(s1_prd_ids, s2_tile_id, out_dirpath_root,
+                        dem_dirpath=None, working_dirpath_root=None,
                         clean=False, upload_outputs=False):
-    
+
     if working_dirpath_root is None:
         working_dirpath_root = Path(tempfile.gettempdir())
 
@@ -31,10 +31,10 @@ def generate_s1_ard_from_pids(s1_prd_ids, s2_tile_id, out_dirpath_root,
     if dem_dirpath is None:
         dem_dirpath = working_dirpath / 'dem' / s2_tile_id
         dem_dirpath.mkdir(exist_ok=True, parents=True)
-        get_srtm1s(s2_tile_id, dem_dirpath)
+        get_srtm1s(s2_tile_id, dem_dirpath, 'local')
 
-    generate_s1_ard(s1_prd_ids, s2_tile_id, out_dirpath_root,  
-                    dem_dirpath, working_dirpath, 
+    generate_s1_ard(s1_prd_ids, s2_tile_id, out_dirpath_root,
+                    dem_dirpath, working_dirpath,
                     clean=clean, upload_outputs=upload_outputs)
 
     if clean:
@@ -66,7 +66,7 @@ def parse_args(args):
     parser.add_argument(dest="out_dirpath", help="Output Dirpath", type=Path)
     parser.add_argument(dest="s1_prd_ids", help="Sentinel-1 Product ids", nargs='*')
     parser.add_argument("--dem_dirpath", dest="dem_dirpath", help="DEM dirpath", type=Path)
-    parser.add_argument("-w", dest="working_dirpath", help="Working dirpath", type=Path, 
+    parser.add_argument("-w", dest="working_dirpath", help="Working dirpath", type=Path,
         default=Path(tempfile.gettempdir()))
     parser.add_argument("--clean", action='store_true', help= 'Clean all dirs')
     parser.add_argument("--upload", action='store_true', help= 'Upload outputs to s3 bucket')
@@ -114,7 +114,7 @@ def main(args):
     args = parse_args(args)
     setup_logging(args.loglevel)
     logger.debug("Starting Generate S1 ARD for %s over %s MGRS Tile ...", args.s1_prd_ids, args.s2_tile_id)
-    generate_s1_ard_from_pids(args.s1_prd_ids, args.s2_tile_id, 
+    generate_s1_ard_from_pids(args.s1_prd_ids, args.s2_tile_id,
                               args.out_dirpath, args.dem_dirpath, args.working_dirpath,
                               args.clean, args.upload)
     logger.info("Generation of S1 ARD for %s over %s MGRS Tile is ended!", args.s1_prd_ids, args.s2_tile_id)
